7ecfd4294844f41d108a72dceec0489a2c39c474
Bruno Araujo <brunoandradel.a.dev@gmail.com>
2024-08-06 17:53:23 -0300

    :tada: Commit inicial

---
+++ src/main/java/fiap/sofftek/sofftek_helper/SofftekHelperApplication.java
@@ -0,0 +1,13 @@
+package fiap.sofftek.sofftek_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofftekHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofftekHelperApplication.class, args);
+	}
+
+}
